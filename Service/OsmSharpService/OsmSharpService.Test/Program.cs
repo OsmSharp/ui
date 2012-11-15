438e848515a1d15ab7df79b6684c81e52d374ebc
xivk_cp <SND\xivk_cp>
2012-11-15 07:31:35 -0800

    Created skelton folders for branching. Updated OsmSharpService.
    Former-commit-id: b07e5a05df1197ad13ec165866bfc61ac7fb98f6

--- Service/OsmSharpService/OsmSharpService.Test/Program.cs
+++ Service/OsmSharpService/OsmSharpService.Test/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using ServiceStack.ServiceClient.Web;
 using OsmSharpService.Core.Routing;
+using System.IO;
 
 namespace OsmSharpService.Test
 {
@@ -12,8 +13,7 @@ namespace OsmSharpService.Test
         static void Main(string[] args)
         {
             JsonServiceClient client = new JsonServiceClient("http://localhost:666/");
-            //2;1551494;51.18221;3.573142
-            //3;1551503;51.18324;3.57229
+
             RoutingHook hook1 = new RoutingHook()
             {
                 Id = 1,
@@ -23,18 +23,23 @@ namespace OsmSharpService.Test
             RoutingHook hook2 = new RoutingHook()
             {
                 Id = 1,
-                Latitude = 51.18324f,
-                Longitude = 3.57229f
+                Latitude = 51.18479f,
+                Longitude = 3.56409f
             };
-
-            for (int idx = 0; idx < 100; idx++)
+            for (int idx = 0; idx < 1000000; idx++)
             {
                 var response = client.Send<RoutingResponse>(
                     new RoutingOperation()
                     {
                         Hooks = new RoutingHook[] { hook1, hook2 },
-                        Type = RoutingOperationType.ManyToMany
+                        Type = RoutingOperationType.Regular
                     });
+
+                if (response.Route != null)
+                {
+                    //response.Route.SaveAsGpx(new FileInfo(string.Format(@"c:\temp\route_{0}.gpx",
+                    //    idx)));
+                }
                 Console.WriteLine(response); // => Hello, World
             }
             Console.ReadLine();
