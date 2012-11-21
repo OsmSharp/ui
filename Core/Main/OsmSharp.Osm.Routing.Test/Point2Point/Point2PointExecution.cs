44042d9a5831d7545dd7b9e3b733907c779ad095
xivk_cp <SND\xivk_cp>
2012-11-21 02:26:16 -0800

    Updated CH to use new priority queue.
    Former-commit-id: 7acce97c0b8239736f5979919e4b31a324cd07aa

--- Core/Main/OsmSharp.Osm.Routing.Test/Point2Point/Point2PointExecution.cs
+++ Core/Main/OsmSharp.Osm.Routing.Test/Point2Point/Point2PointExecution.cs
@@ -18,12 +18,12 @@ namespace OsmSharp.Osm.Routing.Test.Point2Point
         /// </summary>
         public static void Execute()
         {
-            Point2PointExecution.Execute<OsmSharp.Osm.Routing.Data.OsmEdgeData>(
-                new Point2Point.Point2PointDykstraTests());
+            //Point2PointExecution.Execute<OsmSharp.Osm.Routing.Data.OsmEdgeData>(
+            //    new Point2Point.Point2PointDykstraTests());
             Point2PointExecution.Execute<OsmSharp.Osm.Routing.Data.OsmEdgeData>(
                 new Point2Point.Point2PointDykstraBinairyHeapTests());
-            //Point2PointExecution.Execute<OsmSharp.Routing.CH.PreProcessing.CHEdgeData>(
-            //    new Point2Point.Point2PointCHTests());
+            Point2PointExecution.Execute<OsmSharp.Routing.CH.PreProcessing.CHEdgeData>(
+                new Point2Point.Point2PointCHTests());
         }
 
         /// <summary>
@@ -49,8 +49,8 @@ namespace OsmSharp.Osm.Routing.Test.Point2Point
             tester.ExecuteTest("matrix", test_count);
             tester.ExecuteTest("eeklo", test_count);
             tester.ExecuteTest("lebbeke", test_count);
-            tester.ExecuteTest("matrix_big_area", test_count);
-            tester.ExecuteTest("moscow", test_count);
+            //tester.ExecuteTest("matrix_big_area", test_count);
+            //tester.ExecuteTest("moscow", test_count);
             //tester.ExecuteTest("flanders_highway", (new FileInfo(@"c:\OSM\bin\flanders_highway.osm.pbf")).OpenRead(),
             //    true, 100);
 
