ba52009b65123116d9af470b938273f693c5f96e
xivk_cp <SND\xivk_cp>
2012-11-21 07:49:35 -0800

    Updated all assembly versions.
    Former-commit-id: 4151556a425542909b9743454dce45ed92d84eba

--- Core/Main/OsmSharp.Tools.Math/Properties/AssemblyInfo.cs
+++ Core/Main/OsmSharp.Tools.Math/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("3.1.*")]
 // [assembly: AssemblyVersion("3.1.*")]
-// [assembly: AssemblyFileVersion("1.0.0.0")]
+// // [assembly: AssemblyFileVersion("1.0.0.0")]
