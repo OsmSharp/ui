e50c9506e801e63b52587158eb8ca42e2446bad0
xivk_cp <SND\xivk_cp>
2013-03-20 03:16:52 -0700

    Fixed package script.
    Former-commit-id: 5ae220e79c402d75a6ab5c5f04fe19a23ea668cd

--- Core/Releases/v3.1/Core/package.bat
+++ Core/Releases/v3.1/Core/package.bat
@@ -18,4 +18,4 @@ REM ilmerge /target:library /log:OutputPackaged/Log.OsmSharp.Winforms.log /targe
 REM Merging OsmSharp.dll
 ilmerge /target:library /log:OutputPackaged/Log.OsmSharp.log /targetplatform:v4,"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /out:OutputPackaged/OsmSharp.dll Output/OsmSharp.Tools.Core.dll Output/OsmSharp.Tools.GeoCoding.dll Output/OsmSharp.Tools.Math.dll Output/OsmSharp.Tools.Math.AI.Genetic.dll Output/OsmSharp.Tools.Math.TSP.dll Output/OsmSharp.Tools.Math.VRP.Core.dll Output/OsmSharp.Tools.Math.VRP.MultiSalesman.dll Output/OsmSharp.Tools.TSPLIB.dll Output/OsmSharp.Tools.Xml.dll Output/OsmSharp.Osm.Core.dll Output/OsmSharp.Osm.Data.Core.dll Output/OsmSharp.Osm.Data.Oracle.dll Output/OsmSharp.Osm.Data.PBF.dll Output/OsmSharp.Osm.Data.PostgreSQL.dll Output/OsmSharp.Osm.Data.Redis.dll Output/OsmSharp.Osm.Data.XML.dll Output/OsmSharp.Osm.Routing.dll Output/OsmSharp.Routing.Core.dll Output/OsmSharp.Routing.Core.TSP.dll Output/OsmSharp.Routing.Core.VRP.dll Output/OsmSharp.Routing.CH.dll Output/OsmSharp.Routing.Instructions.dll
 
-pause
+exit /b 0
