a855f932238c9b17428d0441300f7fa1b6c6dc33
xivk_cp <SND\xivk_cp>
2013-03-22 09:56:31 -0700

    Change sample output folder. Added automatic installation code.
    Former-commit-id: a89433c67c9f295db285302d7253b93ecbebb59b

---
+++ Samples/TiledDataService/TileDataServiceService/Installer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+
+namespace TileDataServiceService
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+    }
+}
