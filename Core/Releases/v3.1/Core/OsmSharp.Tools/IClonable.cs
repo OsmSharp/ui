e8fbb08b06a39f4951ed1d9a95ff9ff60ea0d0e8
xivk_cp <SND\xivk_cp>
2013-03-23 15:26:26 -0700

    Added WindowsPhone solution.
    Former-commit-id: 46b7ffec8e39428a960f131442667bce43c258d4

---
+++ Core/Releases/v3.1/Core/OsmSharp.Tools/IClonable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if SILVERLIGHT || WINDOWS_PHONE
+
+namespace OsmSharp
+{
+    /// <summary>
+    /// IClonable interface to build for Windows Phone.
+    /// </summary>
+    public interface ICloneable
+    {
+        /// <summary>
+        /// Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// <returns></returns>
+        object Clone();
+    }
+}
+
+#endif
