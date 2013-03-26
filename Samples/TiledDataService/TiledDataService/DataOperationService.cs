0ef02bb44f0317dc214776c5d4ff4ec9371ba6e2
xivk_cp <SND\xivk_cp>
2013-03-26 08:45:22 -0700

    Added some custom http responses.
    Former-commit-id: 526be25e0a55edbb543a3d6c038e2f105fd58017

--- Samples/TiledDataService/TiledDataService/DataOperationService.cs
+++ Samples/TiledDataService/TiledDataService/DataOperationService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using OsmSharp.Osm.Data.Core.Processor;
 using OsmSharp.Osm.Data.Core.Processor.Filter;
 using OsmSharp.Osm.Data.PBF.Raw.Processor;
 using OsmSharp.Osm.Data.XML.Processor;
+using ServiceStack.Common.Web;
 using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
 using OsmSharp.Osm;
@@ -27,63 +29,82 @@ namespace TiledDataService
         [AddHeader(ContentType = "text/plain")]
         public string Get(DataOperation request)
         {
-            string data_path = ConfigurationManager.AppSettings["datapath"];
+            try
+            {
+                string dataPath = ConfigurationManager.AppSettings["datapath"];
 
-            // parse the zoom, x and y.
-            int x, y, zoom;
-            if (!int.TryParse(request.X, out x))
-            { // invalid x.
-                throw new InvalidCastException("Cannot parse x-coordinate!");
+                // parse the zoom, x and y.
+                int x, y, zoom;
+                if (!int.TryParse(request.X, out x))
+                {
+                    // invalid x.
+                    throw new InvalidCastException("Cannot parse x-coordinate!");
+                }
+                if (!int.TryParse(request.Y, out y))
+                {
+                    // invalid y.
+                    throw new InvalidCastException("Cannot parse y-coordinate!");
+                }
+                if (!int.TryParse(request.Zoom, out zoom))
+                {
+                    // invalid zoom.
+                    throw new InvalidCastException("Cannot parse zoom!");
+                }
+
+                // check of the file exists.
+                var pbfFile = new FileInfo(dataPath + request.File + ".pbf");
+                var xmlFile = new FileInfo(dataPath + request.File);
+
+                DataProcessorSource source = null;
+                FileStream sourceStream = null;
+                if (pbfFile.Exists)
+                {
+                    // create PBF source.
+                    sourceStream = pbfFile.OpenRead();
+                    source = new PBFDataProcessorSource(sourceStream);
+                }
+                else if (xmlFile.Exists)
+                {
+                    // create XML source.
+                    sourceStream = xmlFile.OpenRead();
+                    source = new XmlDataProcessorSource(sourceStream);
+                }
+                else
+                {
+                    // oeps! file not found!
+                    throw new FileNotFoundException("File not found!", xmlFile.Name);
+                }
+
+                // create the filter.
+                var tile = new Tile(x, y, zoom);
+                DataProcessorFilter filter = new DataProcessorFilterBoundingBox(tile.Box);
+
+                // create the target.
+                var result = new StringBuilder();
+                var writer = new StringWriter(result);
+                var target = new XmlDataProcessorTarget(writer);
+
+                // execute the processing.
+                target.RegisterSource(filter);
+                filter.RegisterSource(source);
+                target.Pull();
+
+                // close the source stream.
+                sourceStream.Close();
+                sourceStream.Dispose();
+
+                return result.ToString();
             }
-            if (!int.TryParse(request.Y, out y))
-            { // invalid y.
-                throw new InvalidCastException("Cannot parse y-coordinate!");
+            catch (FileNotFoundException ex)
+            {
+                base.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return string.Empty;
             }
-            if (!int.TryParse(request.Zoom, out zoom))
-            { // invalid zoom.
-                throw new InvalidCastException("Cannot parse zoom!");
+            catch (Exception ex)
+            {
+                base.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return string.Empty;
             }
-
-            // check of the file exists.
-            var pbf_file = new FileInfo(data_path + request.File + ".pbf");
-            var xml_file = new FileInfo(data_path + request.File);
-
-            DataProcessorSource source = null;
-            FileStream source_stream = null;
-            if (pbf_file.Exists)
-            { // create PBF source.
-                source_stream = pbf_file.OpenRead();
-                source = new PBFDataProcessorSource(source_stream);
-            }
-            else if (xml_file.Exists)
-            { // create XML source.
-                source_stream = xml_file.OpenRead();
-                source = new XmlDataProcessorSource(source_stream);
-            }
-            else 
-            { // oeps! file not found!
-                throw new FileNotFoundException("File not found!", xml_file.Name);
-            }
-
-            // create the filter.
-            var tile = new Tile(x,y, zoom);
-            DataProcessorFilter filter = new DataProcessorFilterBoundingBox(tile.Box);
-
-            // create the target.
-            var result = new StringBuilder();
-            var writer = new StringWriter(result);
-            var target = new XmlDataProcessorTarget(writer);
-
-            // execute the processing.
-            target.RegisterSource(filter);
-            filter.RegisterSource(source);
-            target.Pull();
-
-            // close the source stream.
-            source_stream.Close();
-            source_stream.Dispose();
-
-            return result.ToString();
         }
     }
 }
