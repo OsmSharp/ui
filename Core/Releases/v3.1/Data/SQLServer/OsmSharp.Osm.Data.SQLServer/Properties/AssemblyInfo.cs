696d7a48727f82017083222ff219bc86ca6a603b
xivk_cp <SND\xivk_cp>
2013-04-03 07:55:01 -0700

    Fixed versioning.
    Former-commit-id: 0e46b6d642e45843ee2b1f91fe7dc9ffe54a5467

--- Core/Releases/v3.1/Data/SQLServer/OsmSharp.Osm.Data.SQLServer/Properties/AssemblyInfo.cs
+++ Core/Releases/v3.1/Data/SQLServer/OsmSharp.Osm.Data.SQLServer/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OsmSharp")]
-[assembly: AssemblyProduct("OsmSharp.Osm.Data.SQLServer")]
-[assembly: AssemblyCopyright("Abelshausen Ben, Alexander Sinitsin")]
+[assembly: AssemblyProduct("OsmSharp")]
+[assembly: AssemblyCopyright("Abelshausen Ben, Alexander Sinitsin 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")] // semantic versioning.
