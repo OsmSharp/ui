eebfe59f325d6a760d488ccbf561fc8ed1a37cfa
xivk_cp <SND\xivk_cp>
2013-04-03 10:55:49 -0700

    Fixed versioning.
    Former-commit-id: fedaaa15120c8bb12e1cec7f8dcddef31149ed4e

--- Core/Releases/v3.1/Core/OsmSharp.Osm/Properties/AssemblyInfo.cs
+++ Core/Releases/v3.1/Core/OsmSharp.Osm/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below :
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("3.1.4834.*")]
