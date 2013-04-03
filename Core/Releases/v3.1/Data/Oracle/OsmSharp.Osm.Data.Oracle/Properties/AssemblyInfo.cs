42c16618143a6059af01285c0d50b9a08d75e97a
xivk_cp <SND\xivk_cp>
2013-04-03 14:50:21 -0700

    Change versioning.
    Former-commit-id: 5f301c0fbbbc2c52b33b82fe87e2ad0e186d940c

--- Core/Releases/v3.1/Data/Oracle/OsmSharp.Osm.Data.Oracle/Properties/AssemblyInfo.cs
+++ Core/Releases/v3.1/Data/Oracle/OsmSharp.Osm.Data.Oracle/Properties/AssemblyInfo.cs
@@ -48,4 +48,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")] // semantic versioning.
+[assembly: AssemblyVersion("1.0.0.9999")] // semantic versioning Major.Minor.Patch.Build (9999 will be updated by CI server)
+[assembly: AssemblyInformationalVersion("Local Build Version")] // do not change this; build server replace this automatically.
