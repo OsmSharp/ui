75ec793084fe8ba899598505d9488c06a1fed282
xivk_cp <SND\xivk_cp>
2013-04-10 01:35:38 -0700

    Updated version numbering. New #release.
    Former-commit-id: 70d7dcb7467eb9d41fe8512728615d931beb3b93

--- Core/Releases/v3.1/Core/OsmSharp.Tools/Properties/AssemblyInfo.cs
+++ Core/Releases/v3.1/Core/OsmSharp.Tools/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.4836.9999")] // semantic versioning Major.Minor.Patch.Build (9999 will be updated by CI server)
+[assembly: AssemblyVersion("3.1.4837.9999")] // semantic versioning Major.Minor.Patch.Build (9999 will be updated by CI server)
 [assembly: AssemblyInformationalVersion("Local Build Version")] // do not change this; build server replace this automatically.
