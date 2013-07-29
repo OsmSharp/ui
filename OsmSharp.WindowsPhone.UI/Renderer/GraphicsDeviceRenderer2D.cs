b4eba57275499b47574164dc7e51551196f546a3
xivk <xivk@osmsharp.com>
2013-07-29 14:12:29 +0200

    Fixed comment spelling.
    Former-commit-id: f4d7d87c21f7f4ab345027cbc7613d199ca17dfc

--- OsmSharp.WindowsPhone.UI/Renderer/GraphicsDeviceRenderer2D.cs
+++ OsmSharp.WindowsPhone.UI/Renderer/GraphicsDeviceRenderer2D.cs
@@ -14,28 +14,93 @@ using Microsoft.Xna.Framework.Graphics;
 namespace PhoneClassLibrary4.Renderer
 {
     /// <summary>
-    /// A renderer implemented agains an XNA-GraphicsDevice.
+    /// A renderer implemented against an XNA-GraphicsDevice.
     /// </summary>
     public class GraphicsDeviceRenderer2D : Renderer2D<GraphicsDevice>
     {
+        /// <summary>
+        /// Holds the view.
+        /// </summary>
+        private View2D _view;
+
+        /// <summary>
+        /// Holds the target.
+        /// </summary>
+        private Target2DWrapper<GraphicsDevice> _target;
+
+        /// <summary>
+        /// Returns the target wrapper.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
         public override Target2DWrapper<GraphicsDevice> CreateTarget2DWrapper(GraphicsDevice target)
         {
-            throw new NotImplementedException();
+            return new Target2DWrapper<GraphicsDevice>(
+                target, target.Viewport.Width, target.Viewport.Height);
         }
 
-        protected override double FromPixels(Target2DWrapper<GraphicsDevice> target, View2D view, double sizeInPixels)
+        /// <summary>
+        /// Returns the size in pixels.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="view"></param>
+        /// <param name="sizeInPixels"></param>
+        /// <returns></returns>
+        protected override double FromPixels(Target2DWrapper<GraphicsDevice> target, 
+            View2D view, double sizeInPixels)
         {
-            throw new NotImplementedException();
+            double scaleX = target.Width / view.Width;
+
+            return sizeInPixels / scaleX;
         }
 
+        /// <summary>
+        /// Transforms the canvas to the coordinate system of the view.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="view"></param>
         protected override void Transform(Target2DWrapper<GraphicsDevice> target, View2D view)
         {
-            throw new NotImplementedException();
+            _target = target;
+            _view = view;
         }
+
+        /// <summary>
+        /// Transforms the x-coordinate to screen coordinates.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        private float TransformX(double x)
+        {
+            return (float)_view.ToViewPortX(_target.Width, x);
+        }
+
+        /// <summary>
+        /// Transforms the y-coordinate to screen coordinates.
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private float TransformY(double y)
+        {
+            return (float)_view.ToViewPortY(_target.Height, y);
+        }
+
+        /// <summary>
+        /// Returns the size in pixels.
+        /// </summary>
+        /// <returns></returns>
+        private float ToPixels(double sceneSize)
+        {
+            double scaleX = _target.Width / _view.Width;
+
+            return (float)(sceneSize * scaleX) * 2;
+        }
+
 
         protected override void DrawBackColor(Target2DWrapper<GraphicsDevice> target, int backColor)
         {
-            throw new NotImplementedException();
+            //target.Target.
+            //target.Target.DrawColor(new global::Android.Graphics.Color(backColor));
         }
 
         protected override void DrawPoint(Target2DWrapper<GraphicsDevice> target, double x, double y, int color, double size)
