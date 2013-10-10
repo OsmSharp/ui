005b1127669787e85c5f20e548c5ab6c1da383dc
xivk <xivk@osmsharp.com>
2013-10-10 22:41:42 +0200

    Updated renderer to be able to handle empty scenes.

--- OsmSharp.UI/Renderer/Renderer2D.cs
+++ OsmSharp.UI/Renderer/Renderer2D.cs
@@ -66,6 +66,12 @@ namespace OsmSharp.UI.Renderer
 	    /// <param name="view">View.</param>
         public bool Render(TTarget orginalTarget, List<Scene2D> scenes, View2D view)
 		{
+            if (scenes == null ||
+                scenes.Count == 0)
+            { // there is nothing to render!
+                return;
+            }
+
 			this.SetRunning (true);
 
 			if (view == null)
