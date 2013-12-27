1a66dba0b5b11f07bff042d48c5c6d3bac12205d
xivk <ben@osmsharp.com>
2013-12-27 21:16:26 +0100

    Updated target frameworks.

--- OsmSharp.Test.Performance/Program.cs
+++ OsmSharp.Test.Performance/Program.cs
@@ -19,7 +19,6 @@
 using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using OsmSharp.WinForms.UI.Renderer;
 
 namespace OsmSharp.Test.Performance
 {
--- OsmSharp.WinForms.UI.Sample/MapControlForm.cs
+++ OsmSharp.WinForms.UI.Sample/MapControlForm.cs
@@ -17,6 +17,7 @@
 // along with OsmSharp. If not, see <http://www.gnu.org/licenses/>.
 
 using OsmSharp.Math.Geo;
+using OsmSharp.Osm.Data.Memory;
 using OsmSharp.UI.Map.Layers;
 using OsmSharp.UI.Map.Styles.MapCSS;
 using OsmSharp.UI.Renderer.Scene;
@@ -69,13 +70,13 @@ namespace OsmSharp.WinForms.UI.Sample
             //scene = merger.BuildMergedScene(scene);
 
             //map.AddLayer(new LayerScene(scene));
-            //var dataSource = MemoryDataSource.CreateFromPBFStream(
-            //    new FileInfo(@"kempen.osm.pbf").OpenRead());
-            //map.AddLayer(new LayerOsm(dataSource, mapCSSInterpreter, map.Projection));
+            var dataSource = MemoryDataSource.CreateFromPBFStream(
+                new FileInfo(@"kempen.osm.pbf").OpenRead());
+            map.AddLayer(new LayerOsm(dataSource, mapCSSInterpreter, map.Projection));
             ////map.AddLayer(new LayerTile(@"http://otile1.mqcdn.com/tiles/1.0.0/osm/{0}/{1}/{2}.png"));
-            map.AddLayer(new LayerScene(
-                Scene2D.Deserialize(new FileInfo(@"kempen-big.osm.pbf.scene.layered").OpenRead(),
-                    true)));
+            //map.AddLayer(new LayerScene(
+            //    Scene2D.Deserialize(new FileInfo(@"kempen-big.osm.pbf.scene.layered").OpenRead(),
+            //        true)));
 
             // set control properties.
             this.mapControl1.Map = map;
