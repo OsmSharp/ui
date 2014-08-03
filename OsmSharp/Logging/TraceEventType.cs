6713d9769f99d1d90599f7a1af8cd9a7528dcf71
xivk <ben@osmsharp.com>
2014-08-03 18:02:48 +0200

    Added some missing comments.

--- OsmSharp/Logging/TraceEventType.cs
+++ OsmSharp/Logging/TraceEventType.cs
@@ -23,10 +23,25 @@ namespace OsmSharp.Logging
     /// </summary>
     public enum TraceEventType
     {
+        /// <summary>
+        /// Critical.
+        /// </summary>
         Critical,
+        /// <summary>
+        /// Error.
+        /// </summary>
         Error,
+        /// <summary>
+        /// Warning.
+        /// </summary>
         Warning,
+        /// <summary>
+        /// Verbose.
+        /// </summary>
         Verbose,
+        /// <summary>
+        /// Information.
+        /// </summary>
         Information
     }
 }
