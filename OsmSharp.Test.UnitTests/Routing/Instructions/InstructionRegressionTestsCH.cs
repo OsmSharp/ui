bdf4cbaea898eb643e30b624c5176c674e0559a9
xivk <ben@osmsharp.com>
2015-01-22 16:58:25 +0100

    Fixed instructions regression tests, now they fail like they should.

--- OsmSharp.Test.UnitTests/Routing/Instructions/InstructionRegressionTestsCH.cs
+++ OsmSharp.Test.UnitTests/Routing/Instructions/InstructionRegressionTestsCH.cs
@@ -51,7 +51,7 @@ namespace OsmSharp.Test.Unittests.Routing.Instructions
             TagsTableCollectionIndex tagsIndex = new TagsTableCollectionIndex();
 
             // do the data processing.
-            var data = new DynamicGraphRouterDataSource<CHEdgeData>(tagsIndex);
+            var data = new DynamicGraphRouterDataSource<CHEdgeData>(new MemoryDirectedGraph<CHEdgeData>(), tagsIndex);
             var targetData = new CHEdgeGraphOsmStreamTarget(
                 data, interpreter, tagsIndex, Vehicle.Car);
             var dataProcessorSource = new XmlOsmStreamSource(
