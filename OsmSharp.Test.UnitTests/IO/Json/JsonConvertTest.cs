3e119da999da5827a1544106f7cb9e35be0ae37b
xivk <ben@osmsharp.com>
2015-05-24 20:51:11 +0000

    Added part of the Newtonsoft library as a built-in json serializer.

---
+++ OsmSharp.Test.UnitTests/IO/Json/JsonConvertTest.cs
@@ -0,0 +1,33 @@
+﻿using OsmSharp.IO.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OsmSharp.Test.Unittests.IO.Json
+{
+    class JsonConvertTest
+    {
+        public void Test1()
+        {
+            var product = new Product();
+            product.Name = "Apple";
+            product.Expiry = new DateTime(2008, 12, 28);
+            product.Sizes = new string[] { "Small" };
+
+            var json = JsonConvert.SerializeObject(product);
+
+            var deserialized = JsonConvert.DeserializeObject<Product>(json);
+            
+        }
+
+        private class Product
+        {
+            public string Name { get; set; }
+
+            public DateTime Expiry { get; set; }
+
+            public string[] Sizes { get; set; }
+        }
+    }
+}
