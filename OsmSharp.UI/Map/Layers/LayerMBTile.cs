7b81542cd4880f1bf05cb59e62777b2c7e3b2ddb
xivk <ben@osmsharp.com>
2016-02-03 21:53:28 +0000

    Refactored to latest OsmSharp.Core.

--- OsmSharp.UI/Map/Layers/LayerMBTile.cs
+++ OsmSharp.UI/Map/Layers/LayerMBTile.cs
@@ -1,5 +1,5 @@
 ﻿// OsmSharp - OpenStreetMap (OSM) SDK
-// Copyright (C) 2013 Abelshausen Ben
+// Copyright (C) 2016 Abelshausen Ben
 // 
 // This file is part of OsmSharp.
 // 
@@ -16,18 +16,15 @@
 // You should have received a copy of the GNU General Public License
 // along with OsmSharp. If not, see <http://www.gnu.org/licenses/>.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using OsmSharp.UI.Renderer;
 using OsmSharp.Math.Geo;
 using OsmSharp.Osm.Tiles;
-using OsmSharp.Data.SQLite;
 using OsmSharp.Collections.Cache;
 using OsmSharp.UI.Renderer.Primitives;
 using OsmSharp.Math.Geo.Projections;
 using OsmSharp.UI.Renderer.Images;
+using OsmSharp.UI.Data.SQLite;
 
 namespace OsmSharp.UI.Map.Layers
 {
